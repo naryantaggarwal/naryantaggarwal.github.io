d224b7d48148d80ea7f7a9b147621eb2a4b71850
Naryan Aggarwal <162486108+naryantaggarwal@users.noreply.github.com>
2024-11-08 12:00:56 -0600

    Update scripts.js
    Updated scroll function

--- scripts.js
+++ scripts.js
@@ -9,16 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 window.addEventListener("scroll", function() {
-    var navbar = document.getElementById("navbar");
-    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-    
-    if (viewportWidth > 768) { // Only add .sticky for larger viewports
-        if (window.scrollY > 0) {
-            navbar.classList.add("sticky");
-        } else {
-            navbar.classList.remove("sticky");
-        }
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky"); // Always horizontal on small viewports
+        navbar.classList.remove("sticky");
     }
 });
