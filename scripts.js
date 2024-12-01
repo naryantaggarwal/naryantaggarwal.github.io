5dc7db578cb3f8750de6758b06b2c44810cb266f
Naryan Aggarwal <162486108+naryantaggarwal@users.noreply.github.com>
2024-12-01 10:21:09 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,12 +7,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
     observer.observe(navbar);
 });
-
-window.addEventListener("scroll", function() {
-    const navbar = document.getElementById("navbar");
-    if (window.scrollY > 0) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-});
