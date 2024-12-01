d813a59c2bf10ac41f628bd94c835b7940f2e2ea
Naryan Aggarwal <162486108+naryantaggarwal@users.noreply.github.com>
2024-12-01 10:24:33 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('navbar');
-    const observer = new IntersectionObserver(
-        ([e]) => e.target.classList.toggle('sticky', e.intersectionRatio < 1),
-        { threshold: [1] }
-    );
 
-    observer.observe(navbar);
-});
